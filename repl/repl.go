dd68ef3ca9a83782671ec40d279d70f070d4096e
Vít Chrubasík <me@vichr.me>
2026-02-13 22:50:58 +0100

    feat: e in repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"vichr.me/go/monkey-lang-interpreter/evaluator"
 	"vichr.me/go/monkey-lang-interpreter/lexer"
 	"vichr.me/go/monkey-lang-interpreter/parser"
 )
@@ -30,8 +31,15 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+
+		// this is just for testing the parser, we will remove it later when we implement the evaluator
+		// io.WriteString(out, program.String())
+		// io.WriteString(out, "\n")
 	}
 }
 

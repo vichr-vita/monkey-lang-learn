7e27385c3422ee385e2668ecd33b45c1cfe76a7a
Vít Chrubasík <me@vichr.me>
2026-02-13 20:24:31 +0100

    fix: remove never nil checks

--- parser/parser.go
+++ parser/parser.go
@@ -87,9 +87,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 
 	for !p.curTokenIsType(token.EOF) {
 		stmt := p.parseStatement()
-		if stmt != nil {
-			program.Statements = append(program.Statements, stmt)
-		}
+		program.Statements = append(program.Statements, stmt)
 		p.nextToken()
 	}
 
@@ -231,9 +229,7 @@ func (p *Parser) parseBlockStatement() *ast.BlockStatement {
 
 	for !p.curTokenIsType(token.RBRACE) && !p.curTokenIsType(token.EOF) {
 		stmt := p.parseStatement()
-		if stmt != nil {
-			block.Statements = append(block.Statements, stmt)
-		}
+		block.Statements = append(block.Statements, stmt)
 		p.nextToken()
 	}
 	return block
